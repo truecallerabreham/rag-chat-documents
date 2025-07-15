b1b10753f465f284237288c5e19c613f94b135de
truecallerabreham <truecallerabreham@gmail.com>
2025-07-15 04:08:05 -0700

    Update retriever.py

--- backend/retriever.py
+++ backend/retriever.py
@@ -1,17 +1,20 @@
-from backend/vector_store import search_vector_store
 import os
 from dotenv import load_dotenv
 from groq import Groq
-import openai
+
 load_dotenv()
 api_key = os.getenv("GROQ_API_KEY")
-client=Groq(api_key)
+
+client = Groq(api_key=api_key)
+
 def callllm(prompt):
-     response=client.chat.completions.create(
-         model="mixtral-8x7b-32768",
-         messages=[{"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "user", "content": prompt}],
-         temperature=0.3
-     )
-     return response.choices[0].message.content
-     
+    response = client.chat.completions.create(
+        model="mixtral-8x7b-32768",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.3
+    )
+    return response.choices[0].message.content
+
